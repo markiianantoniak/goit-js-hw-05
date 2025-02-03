63fc93e0500994506abb8c558291baa3ee7f1ffc
= <=>
2025-02-03 01:10:03 -0500

    small fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 const getTotalBalanceByGender = (users, gender) =>
   users
     .filter((user) => user.gender == gender)
-    .reduce((balance, user) => balance + user.balance);
+    .reduce((balance, user) => balance + user.balance, 0);
 const clients = [
   {
     name: "Moore Hensley",
